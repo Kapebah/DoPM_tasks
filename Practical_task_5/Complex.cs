555c1defd5a72b001eca96aea5af2130104b341a
Kapebah <sh1n0sh1@mail.ru>
2024-03-20 17:20:13 +0200

    practical_task_5

---
+++ Practical_task_5/Complex.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_task_5
+{
+    class Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+        public string Name { get; set; } // Дополнительное поле для имени комплексного числа
+        public bool IsReal { get; set; } // Дополнительное поле для определения, является ли число действительным
+        public double Magnitude { get { return Math.Sqrt(Real * Real + Imaginary * Imaginary); } } // Дополнительное поле для определения модуля комплексного числа
+
+        public Complex(double real, double imaginary, string name, bool isReal = false)
+        {
+            Real = real;
+            Imaginary = imaginary;
+            Name = name;
+            IsReal = isReal;
+        }
+
+        public Complex Add(Complex other)
+        {
+            return new Complex(Real + other.Real, Imaginary + other.Imaginary, "");
+        }
+
+        public Complex Subtract(Complex other)
+        {
+            return new Complex(Real - other.Real, Imaginary - other.Imaginary, "");
+        }
+
+        public Complex Multiply(Complex other)
+        {
+            return new Complex(Real * other.Real - Imaginary * other.Imaginary,
+                               Real * other.Imaginary + Imaginary * other.Real, "");
+        }
+
+        public Complex Divide(Complex other)
+        {
+            double denominator = other.Real * other.Real + other.Imaginary * other.Imaginary;
+            return new Complex((Real * other.Real + Imaginary * other.Imaginary) / denominator,
+                               (Imaginary * other.Real - Real * other.Imaginary) / denominator, "");
+        }
+
+        public override string ToString()
+        {
+            if (Imaginary < 0)
+                return $"{Real} - {-Imaginary}i";
+            else
+                return $"{Real} + {Imaginary}i";
+        }
+    }
+}
---
+++ Practical_task_5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Practical_task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Complex a = new Complex(2, 3, "a", false);
+                Complex b = new Complex(1, -1, "b", false);
+
+                WriteLine($"a = {a},\t b = {b}");
+
+                Complex sum = a.Add(b);
+                Complex difference = a.Subtract(b);
+                Complex product = a.Multiply(b);
+                Complex quotient = a.Divide(b);
+
+                WriteLine($"\nSum: {sum}");
+                WriteLine($"Difference: {difference}");
+                WriteLine($"Product: {product}");
+                WriteLine($"Quotient: {quotient}");
+
+                WriteLine($"\nMagnitude of a: {a.Magnitude:f2}");
+                WriteLine($"Magnitude of b: {b.Magnitude:f2}");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+            ReadKey();
+        }
+    }
+}
